5db20a4c8a142b2531ebf8cab78031ea2b300aca
Mr. Lewis <106601886+rafael-25@users.noreply.github.com>
2023-10-14 17:25:44 -0500

    type.py
    Student Notes

---
+++ type.py
@@ -0,0 +1,16 @@
+# Check the data types of different values
+print(type("Hello, world"))
+print(type(10))
+print(type(-3.5))
+print(type(False))
+
+# Define variables
+x = 45
+t = True
+
+# Check the data type of the variables
+print(type(x))
+print(type(t))
+
+print(type("1010"))
+print(type("True"))
