156c5c1257c85ddf08f2bd2f11e163c8a7af36eb
Mr. Lewis <106601886+rafael-25@users.noreply.github.com>
2023-10-15 10:21:44 -0500

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,12 @@
+# Initial values
+a = 5
+b = 10
+
+# Swapping the values
+temp = a
+a = b
+b = temp
+
+# Now, 'a' and 'b' have swapped values
+print("a =", a)  # Output: a = 10
+print("b =", b)  # Output: b = 5
