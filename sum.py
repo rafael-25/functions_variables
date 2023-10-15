4558031c2c1bc8aaded40c6025647a63e47517f8
Mr. Lewis <106601886+rafael-25@users.noreply.github.com>
2023-10-15 10:06:20 -0500

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,11 @@
+print("This program adds two numbers.")
+
+# Get the two numbers from the user
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Calculate the sum
+total = num1 + num2
+
+# Display the results
+print(f"{num1} + {num2} = {total}")
