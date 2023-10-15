5119c85b2111633ae01f7d94a1a3ef5d6d60cc87
Mr. Lewis <106601886+rafael-25@users.noreply.github.com>
2023-10-15 10:13:52 -0500

    Create average.py

---
+++ average.py
@@ -0,0 +1,11 @@
+print("This program averages two numbers")
+
+# Get x and y from the user
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+# Calculate the average
+average = (x + y) / 2
+
+# Display the average
+print(f"The average is {average}.")
