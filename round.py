0554f4842f67816fcfc15e046998e0e4c6f91857
Mr. Lewis <106601886+rafael-25@users.noreply.github.com>
2023-10-30 07:02:32 -0500

    Create round.py

---
+++ round.py
@@ -0,0 +1,9 @@
+# Get two values from the user
+n = float(input("What is n? "))
+d = float(input("What is d? "))
+
+# Round the expression to 2 decimal points.
+q = round(n / d,2)
+
+# Print the quotient
+print(q)
