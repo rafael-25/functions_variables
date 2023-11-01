b90c9516db33d454b49d1e0d990de1a56f82230c
Mr. Lewis <106601886+rafael-25@users.noreply.github.com>
2023-11-01 10:38:56 -0500

    Create trapezoid.py

---
+++ trapezoid.py
@@ -0,0 +1,14 @@
+"""
+Create an interactive program that:
+
+    (1) Asks the user for the length of the first parallel side of the trapezoid.
+
+    (2) Asks the user for the length of the second parallel side of the trapezoid.
+
+    (3) Asks the user for the height of the trapezoid.
+
+    (4) Calculates the area of the trapezoid using the formula:
+        Area = 0.5 * (side1 + side2) * height
+
+    (5) Displays the calculated area, rounded to two decimal places.
+"""
