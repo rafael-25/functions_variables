33866677d4ec9cbec99f8f0649715058326644f0
Mr. Lewis <106601886+rafael-25@users.noreply.github.com>
2023-11-04 11:20:55 -0500

    Update pro_hello.py

--- pro_hello.py
+++ pro_hello.py
@@ -1,7 +1,8 @@
 def hello(to="world"):
+    """Print a greeting to a recipient or 'world' if no recipient is provided."""
     print("hello," + to)
 
-name = input("What's your name?")
+name = input("What's your name? ")
 hello(name)
 hello()
 print("Program End")
